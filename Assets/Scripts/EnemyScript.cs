4f86f725f752d8097108c25672a37d6f10dbdfb2
UpSetSummerSet <72717739+UpSetSummerSet@users.noreply.github.com>
2021-01-19 00:04:36 +0800

    Continue

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -1,28 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyScript : MonoBehaviour
 {
+    public Text score;
+    int currentpoints;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "Bullet")
-        {
-            Destroy(gameObject);
-            Destroy(collision.gameObject);
-        }
+        score.GetComponent<Text>().text = "Score: " + currentpoints;
     }
 
     private void OnTriggerStay(Collider other)
@@ -31,6 +26,7 @@ public class EnemyScript : MonoBehaviour
         {
             Destroy(gameObject);
             Destroy(other.gameObject);
+            currentpoints += 100;
         }
     }
 }
